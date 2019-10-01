0a65b4a7e84dc6470726a4702d5918dfacfda484
Ryo Ikarashi <ryo.5630@gmail.com>
2019-10-01 17:42:35 +0900

    add a tokenizer to allow whitespaces

--- ccc.c
+++ ccc.c
@@ -1,6 +1,105 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <stdbool.h>
+
+typedef enum {
+    TK_RESERVED, // symbol
+    TK_NUM, // integer
+    TK_EOF, // EOF
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenKind kind; // type of token
+    Token *next; // next token
+    int val; // value of TK_NUM token
+    char *str; // strings of token
+};
+
+Token *token; // current token
+
+// reports an error
+void error(char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+// if the next token is an expected symbol, then assigns next token and returns true
+// otherwise returns false
+bool consume(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+        return false;
+    token = token->next;
+    return true;
+}
+
+// if the next token is an expected symbol, then assigns next token
+// otherwise reports an error
+void expect(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+        error("Expected '%c'", op);
+    token = token->next;
+}
+
+// if the next token is an integer, then assigns next token
+// otherwise reports an error
+int expect_number() {
+    if (token->kind != TK_NUM)
+        error("Not integer");
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof() {
+    return token->kind == TK_EOF;
+}
+
+// create a new token and assign a newly created token to cur's next token
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    cur->next = tok;
+    return tok;
+}
+
+// tokenize input p string and return it
+Token *tokenize(char *p) {
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while (*p) {
+        // skip empty string
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-') {
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = (int) strtol(p, &p, 10);
+            continue;
+        }
+
+        error("Cannot tokenize");
+    }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
+}
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -8,28 +107,25 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    char *p = argv[1];
+    // tokenize
+    token = tokenize(argv[1]);
 
+    // output the first part of assembly
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf(" mov rax, %ld\n", strtol(p, &p, 10));
 
-    while (*p)  {
-        if (*p == '+') {
-            p++;
-            printf(" add rax, %ld\n", strtol(p, &p, 10));
+    // check if the first string is integer, and assign it
+    printf(" mov rax, %d\n", expect_number());
+
+    while (!at_eof()) {
+        if (consume('+')) {
+            printf(" add rax, %d\n", expect_number());
             continue;
         }
 
-        if (*p == '-') {
-            p++;
-            printf(" sub rax, %ld\n", strtol(p, &p, 10));
-            continue;
-        }
-
-        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
-        return 1;
+        expect('-');
+        printf(" sub rax, %d\n", expect_number());
     }
 
     printf(" ret\n");
