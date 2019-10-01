a329bbfae2324fe219335ad218d2ed1d54e729c3
Ryo Ikarashi <ryo.5630@gmail.com>
2019-10-01 16:09:41 +0900

    fix wrong type

--- ccc.c
+++ ccc.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,18 +13,18 @@ int main(int argc, char **argv) {
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf(" mov rax, %d\n", strtol(p, &p, 10));
+    printf(" mov rax, %ld\n", strtol(p, &p, 10));
 
     while (*p)  {
         if (*p == '+') {
             p++;
-            printf(" add rax, %d\n", strtol(p, &p, 10));
+            printf(" add rax, %ld\n", strtol(p, &p, 10));
             continue;
         }
 
         if (*p == '-') {
             p++;
-            printf(" sub rax, %d\n", strtol(p, &p, 10));
+            printf(" sub rax, %ld\n", strtol(p, &p, 10));
             continue;
         }
 
